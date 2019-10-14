f5741a2b98020bd37a886ebc41ff2c92c86088c0
sw1t4er <31046202+sw1t4er@users.noreply.github.com>
2019-10-14 15:04:56 +0300

    14.10

--- DZ1/src/main/java/MyArrayList.java
+++ DZ1/src/main/java/MyArrayList.java
@@ -3,8 +3,15 @@ import java.util.List;
 
 public class MyArrayList<E> extends AbstractList<E> implements List<E> {
 
+    private final int INIT_SIZE = 10;
+
     private Object[] array;
-    private int size;
+    private int pointer = 0;
+
+    public MyArrayList() {
+        this.array =  new Object[INIT_SIZE];
+        this.pointer =0;
+    }
 
     public MyArrayList(int initialCapacity) {
         super();
@@ -12,45 +19,41 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E> {
             throw new IllegalArgumentException("Illegal Capacity: "+
                     initialCapacity);
         this.array = new Object[initialCapacity];
-        size = array.length;
-    }
-
-    public MyArrayList() {
-        this(10);
+        pointer = array.length;
     }
 
     // Возвращает элемент списка по индексу.
     @Override
     public E get(int index) {
-        if (index >= size || index < 0)
-            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
+        if (index >= pointer || index < 0)
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+pointer);
         return (E) array[index];
     }
 
     @Override
     public int size() {
-        return size;
+        return pointer;
     }
 
     // Добавляет новый элемент в список. При достижении размера внутреннего массива происходит его увеличение
     // в (oldCapacity * 3) / 2 + 1 раза.
     @Override
     public boolean add(E value){
-        if(size == array.length-1)
+        if(pointer == array.length-1)
             resize( ((array.length*3)/2)+1 ); // увеличивает в (oldCapacity * 3) / 2 + 1 раза, если достигли границ
-        array[size++] = value;
+        array[pointer++] = value;
         return true;
     }
 
     public void add(E value, int index) {
-        if (index > size || index < 0)
-            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
-        if(size == array.length-1)
+        if (index > pointer || index < 0)
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+pointer);
+        if(pointer == array.length-1)
             resize( ((array.length*3)/2)+1 ); // увеличивает в (oldCapacity * 3) / 2 + 1 раза, если достигли границ
         System.arraycopy(array, index, array, index + 1,
-                size - index);
+                pointer - index);
         array[index] = value;
-        size++;
+        pointer++;
     }
 
     // Добавляет новый массив в список. При достижении размера внутреннего массива происходит его увеличение
@@ -59,14 +62,14 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E> {
         Object[] a = list.toArray();
         int numNew = a.length;
         resize( ((array.length*3)/2) + numNew );  // увеличивает на (oldCapacity * 3) / 2 и размер добавленного массива
-        System.arraycopy(a, 0, array, size, numNew);
-        size += numNew;
+        System.arraycopy(a, 0, array, pointer, numNew);
+        pointer += numNew;
         return numNew != 0;
     }
 
     public E set(E value, int index) {
-        if (index >= size)
-            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
+        if (index >= pointer)
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+pointer);
         E oldValue = array(index);
         array[index] = value;
         return oldValue;
@@ -74,25 +77,25 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E> {
 
     // Возвращает элемент, который удалили.
     @Override
-    public E remove(int index) {
-        if (index >= size)
-            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
+    public E remove(int index) throws IndexOutOfBoundsException {
+        if (index >= pointer)
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+pointer);
         E oldValue = array(index);
-        int numMoved = size - index - 1;
+        int numMoved = pointer - index - 1;
         if (numMoved > 0)
             System.arraycopy(array, index+1, array, index, numMoved);
-        array[--size] = null;
+        array[--pointer] = null;
         return oldValue;
     }
 
     @Override
     public boolean remove(Object t) {
-        for (int index = 0; index < size; index++)
+        for (int index = 0; index < pointer; index++)
             if (t.equals(array[index])) {
-                int numMoved = size - index - 1;
+                int numMoved = pointer - index - 1;
                 if (numMoved > 0)
                     System.arraycopy(array, index+1, array, index, numMoved);
-                array[--size] = null;
+                array[--pointer] = null;
                 return true;
             }
         return false;
@@ -101,14 +104,25 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E> {
     // Вспомогательный метод для масштабирования.
     private void resize(int newLength) {
         Object[] newArray = new Object[newLength];
-        System.arraycopy(array, 0, newArray, 0, size);
+        System.arraycopy(array, 0, newArray, 0, pointer);
         array = newArray;
     }
 
     private E array(int index) {
-        if (index >= size || index < 0)
-            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
+        if (index >= pointer || index < 0)
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+pointer);
         return (E) array[index];
+    }
+
+    @Override
+    public String toString() {
+        String result="{[";
+        for (int i = 0; i < pointer; i++) {
+            result+="{"+i+": \""+array[i].toString()+"\"},";
+        }
+        result=result.substring(0,result.length()-1);
+        result+="]}";
+        return result;
     }
 
 }
